3a6273e036e847f381d62037ba0207387920c4ca
MonTea-lh <haouch.lahcen@gmail.com>
2017-10-30 20:51:46 +0100

    [cra-#6#] Adding rest resource for the absence creation functionality (without implementation). Adding test unit for the rest absence creation service (At this point, the test should fail of course).

---
+++ src/main/java/com/zsoft/cra/web/rest/AbsenceResource.java
@@ -0,0 +1,40 @@
+package com.zsoft.cra.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import com.zsoft.cra.domain.Absence;
+import com.zsoft.cra.repository.AbsenceRepository;
+import com.zsoft.cra.security.AuthoritiesConstants;
+import com.zsoft.cra.service.AbsenceService;
+import com.zsoft.cra.service.dto.AbsenceDTO;
+import com.zsoft.cra.web.rest.util.HeaderUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+@RestController
+@RequestMapping("/api")
+public class AbsenceResource {
+
+    private final Logger logger = LoggerFactory.getLogger(AbsenceResource.class);
+    private final AbsenceRepository absenceRepository;
+    private final AbsenceService absenceService;
+    private final static String PREFIX = "/api/absences/";
+
+    public AbsenceResource(AbsenceRepository absenceRepository, AbsenceService absenceService) {
+        this.absenceRepository = absenceRepository;
+        this.absenceService = absenceService;
+    }
+
+    @PostMapping("/absence")
+    public ResponseEntity createUser(@RequestBody AbsenceDTO absenceDTO) throws URISyntaxException {
+
+    }
+}
---
+++ src/test/java/com/zsoft/cra/web/rest/AbsenceResourceTest.java
@@ -0,0 +1,113 @@
+package com.zsoft.cra.web.rest;
+
+import com.zsoft.cra.CraApp;
+import com.zsoft.cra.domain.Absence;
+import com.zsoft.cra.domain.AbsenceType;
+import com.zsoft.cra.domain.User;
+import com.zsoft.cra.repository.AbsenceRepository;
+import com.zsoft.cra.repository.UserRepository;
+import com.zsoft.cra.service.AbsenceService;
+import com.zsoft.cra.service.dto.AbsenceDTO;
+import com.zsoft.cra.web.rest.errors.ExceptionTranslator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static com.zsoft.cra.domain.AbsenceType.UNJUSTIFIED_ABSENCE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Test class for the AbsenceResource REST controller.
+ *
+ * @see AbsenceResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CraApp.class)
+public class AbsenceResourceTest {
+
+    @Autowired
+    private final AbsenceRepository absenceRepository;
+
+    @Autowired
+    private final AbsenceService absenceService;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    private MockMvc restAbsenceMockMvc;
+
+    private Absence absence;
+    private LocalDate date;
+    private static String ID = "absence";
+    private static String COMMENT = "unjustified absence";
+
+    private User user;
+    private static String USER_LOGIN = "user";
+
+    @Before
+    public void setUp() {
+
+        MockitoAnnotations.initMocks(this);
+        AbsenceResource absenceResource = new AbsenceResource(absenceRepository, absenceService);
+        restAbsenceMockMvc = MockMvcBuilders.standaloneSetup(absenceResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setMessageConverters(jacksonMessageConverter)
+            .build();
+    }
+
+    @Test
+    public void createAbsenceTest() throws Exception {
+
+        int databaseSizeBeforeCreate = absenceRepository.findAll().size();
+
+        AbsenceDTO absenceDTO = new AbsenceDTO(
+            ID,
+            date,
+            date.plus(1, ChronoUnit.DAYS),
+            UNJUSTIFIED_ABSENCE,
+            COMMENT,
+            user
+        );
+
+        restAbsenceMockMvc.perform(post("/api/absence")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(absenceDTO)))
+            .andExpect(status().isCreated());
+
+        List<Absence> absencesList = absenceRepository.findAll();
+        assertThat(absencesList).hasSize(databaseSizeBeforeCreate + 1);
+
+        Absence testAbsence = absencesList.get(absencesList.size() - 1);
+
+        assertThat(testAbsence.getId()).isEqualTo(ID);
+        assertThat(testAbsence.getBeginningDate()).isEqualTo(date);
+        assertThat(testAbsence.getEndingDate()).isEqualTo(date.plus(1, ChronoUnit.DAYS));
+        assertThat(testAbsence.getComment()).isEqualTo(COMMENT);
+        assertThat(testAbsence.getUser().getLogin()).isEqualTo(USER_LOGIN);
+    }
+
+}
