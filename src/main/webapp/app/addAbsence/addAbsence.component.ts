0e760d9adb69fda6bbbfee2b19af56b8c5ac02c3
BilelHimeur <bilelhimeur@gmail.com>
2018-07-11 16:56:06 +0200

    addAbsence and displayAbsence components

---
+++ src/main/webapp/app/addAbsence/addAbsence.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector : 'addAbsence',
+    templateUrl : './addAbsence.component.html',
+    styleUrls : ['./addAbsence.scss']
+
+})
+
+
+export class AddAbsenceComponent implements OnInit {
+
+    ngOnInit() {
+    }
+
+}
+
+
